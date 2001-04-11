fe19f7f00e38f3c8fcde4be12007d24111c0c330
achim <achim@4362c293-2511-481e-a17b-8864836be22b>
2001-04-11 13:00:13 +0000

    necessary changes for autogen.sh and ghmm-config

--- trunk/ghmm/autogen.sh
+++ trunk/ghmm/autogen.sh
@@ -1,14 +1,13 @@
-#!/bin/sh
+#! /bin/sh
 
 #clean up
-rm -f aclocal.m4 configure Makefile.in Makefile stamp-h.in stamp-h \
+rm -f aclocal.m4 configure \
+Makefile.in stamp-h.in config.h.in \
 mkinstalldirs missing install-sh \
-config.h config.h.in \
-config.cache config.log config.status \
-doc/Makefile doc/Makefile.in doc/docu.dxx \
-ghmm/Makefile ghmm/Makefile.in \
-tests/Makefile tests/Makefile.in \
-tools/Makefile tools/Makefile
+doc/Makefile.in \
+ghmm/Makefile.in \
+tests/Makefile.in \
+tools/Makefile.in
 
 #makes aclocal.m4 from acinclude.m4 and other files
 aclocal
