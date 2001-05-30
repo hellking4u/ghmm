2ef26e02e3954e33dd022fdfe562924bc5f46464
achim <achim@4362c293-2511-481e-a17b-8864836be22b>
2001-05-30 21:16:21 +0000

    better detection of blas linker options m4 preprocessor selection enhanced

--- trunk/ghmm/autogen.sh
+++ trunk/ghmm/autogen.sh
@@ -4,6 +4,10 @@
 #  created      : DATE: April 2001
 #  $Id$
 
+#move GNU m4 to head of PATH
+for f in `type -ap m4` ; do
+  echo `eval $f --version`|grep GNU >/dev/null && PATH=`dirname $f`:$PATH ;
+done
 
 #makes aclocal.m4 from acinclude.m4 and other files
 aclocal
