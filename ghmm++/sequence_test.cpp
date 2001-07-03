78a95d2cbe938fc5f5d3269b6fc83eed09918291
achim <achim@4362c293-2511-481e-a17b-8864836be22b>
2001-07-03 09:09:54 +0000

    new deriviations of objects from stl

--- ghmm++/sequence_test.cpp
+++ ghmm++/sequence_test.cpp
@@ -1,10 +1,30 @@
 #include "XMLIO_ArrayObject.h"
 
+int main_ArrayObject()
+{
+  XMLIO_ArrayObject<double> my_array;
+  my_array.XMLIO_getCharacters("1.3 1.4 1e4 1 f2dsdf");
+  my_array.push_back(0);
+  my_array.print();
+
+  XMLIO_ArrayObject<string> my2_array;
+  my2_array.XMLIO_getCharacters("1.3 1.4 1e4 1 f2dsdf");
+  my2_array.print();
+  
+  XMLIO_ArrayObject<char> my3_array;
+  my3_array.XMLIO_getCharacters("1.3 1.4 1e4 1 f2dsdf");
+  my3_array.print();
+
+  return 0;
+}
+
+int main_PairObject()
+{
+  XMLIO_ContentElementPairObject<int,XMLIO_StringObject> pair_vector;
+  return 0;
+}
+
 int main()
 {
-  XMLIO_Array<double> my_array;
-  my_array.XMLIO_getCharacters("1 1 1 1");
-  my_array.push_back(1);
-  my_array.print();
-  return 0;
+  return main_PairObject();
 }
