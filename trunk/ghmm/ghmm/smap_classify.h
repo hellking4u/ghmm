27f8bd713055c5470519067c14fa88e2118d4435
disa <disa@4362c293-2511-481e-a17b-8864836be22b>
2001-07-20 13:29:05 +0000

    Comments in English

--- trunk/ghmm/ghmm/smap_classify.h
+++ trunk/ghmm/ghmm/smap_classify.h
@@ -21,11 +21,35 @@ extern "C" {
 /*@{ */
 
 /**
+   Maximum A Posteriori Classification Algorithm (MAPCA): 
+   given a field of models and one sequence and suppose the sequence
+   has been produced by one of these models. This algorithm calculates
+   for each model the probability, that the seq. comes from the model.
+   This bayesian approach uses a prior for the models. If none is specified
+   equal prob. is assumed.
+   The maps are copied into "result", which has to be of dimension "smo_number"
+   Ref.: A. Kehagias: Bayesian Classification of HMM, Math. Comp. Modelling
+   (1995)
+   @return number of the model, that fits best to the sequence
+   @param smo vector of models
+   @param result gives the probability for all the models
+   @param smo_number number of models
+   @param O sequence
+   @param T length of the sequence
  */
 int smap_classify(smodel **smo, double *result, int smo_number, 
 		   double *O, int T);
 
 /**
+   Alternative to MAPCA (smap_classify); calculate p[m] directly using 
+   Bayes' theorem, instead of recursive over t.
+   p(m | O) = p(O | m) * p(m) / (sum_i p(O | i) * p(i))
+   @return number of the model, that fits best to the sequence
+   @param smo vector of models
+   @param result gives the probability for all the models
+   @param smo_number number of models
+   @param O sequence
+   @param T length of the sequence
  */
 int smap_bayes(smodel **smo, double *result, int smo_number, double *O, int T);
 
