940c24bcbc7d916c69490c1d68966eaaf3c1931a
achim <achim@4362c293-2511-481e-a17b-8864836be22b>
2002-02-24 20:37:26 +0000

    the test from David Posada for sequences read failure

---
+++ trunk/ghmm/tests/sequences_old_format.c
@@ -0,0 +1,48 @@
+/*
+  author       : David Posada <dposada@variagenics.com>
+  filename     : ghmm/tests/sequences_old_format.c
+  created      : DATE: Februar 2002
+  $Id$
+  __copyright__
+*/
+
+/* should be corrected with ghmm/sequence.c version 1.9 */
+#include <stdio.h>
+#include <ghmm/sequence.h>
+   
+int main()
+{
+    int test_result=0;
+    const char* double_sequences_file="data/test100.sqd";
+    sequence_d_t **sqd = NULL;
+    int sqd_number;
+    const char* int_sequences_file="data/sequences_old_format.sq";    
+    sequence_t **data = NULL;
+    int data_number;
+
+    /* read double sequences (this works fine)*/
+    fprintf(stderr,"reading double sequences from %s ...",double_sequences_file);
+    sqd=sequence_d_read((char*)double_sequences_file, &sqd_number);
+    if (sqd==NULL) {
+      test_result=1;
+      fprintf(stdout, " Failed\n");
+    }
+    else {
+      fprintf(stdout," Done\n");
+      sequence_d_free(sqd);
+    }
+
+
+     /* read int sequences (this gives a segmentation fault)*/
+    fprintf(stderr,"reading int sequences from %s ...",int_sequences_file);
+    data=sequence_read((char*)int_sequences_file,&data_number);
+    if (data==NULL) {
+      test_result=1;
+      fprintf(stdout, " Failed\n");
+    }
+    else {
+      fprintf(stdout," Done\n");
+      sequence_free(data);
+    }
+    return test_result;
+}
