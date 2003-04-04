e1adb1c77db1c79c9a24e54d1d11994aea9760ec
schliep <schliep@4362c293-2511-481e-a17b-8864836be22b>
2003-04-04 11:55:04 +0000

    Model type flags ...

---
+++ trunk/ghmm/ghmm/ghmm.h
@@ -0,0 +1,43 @@
+/*-----------------------------------------------------------------------------
+  author       : Alexander Schliep
+  filename     : ghmm.h
+  created      : TIME: 21:44:45     DATE: Sun 14. November 1999
+  $Id$
+
+__copyright__
+
+------------------------------------------------------------------------------*/
+
+#ifndef GHMM_H
+#define GHMM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/**@name GHMM-Globals */
+/*@{ (Doc++-Group: globals) */
+#ifdef __cplusplus
+}
+#endif
+
+
+
+/** @name type_constants
+    Constants giving model variations */
+/** Model is a left-right */
+#define kLeftRight (1);
+/** Model contains silent states (i.e., states without emissions) */
+#define kSilentStates (1 << 2);
+/** Model has states with tied emission probabilities */
+#define kTiedEmissions (1 << 3);
+#define kUntied -1 
+
+/** Model has states emission probabilities conditioned on previous orders */
+#define kHigherOrderEmissions (1 << 4);
+
+
+
+#endif
+
+/*@} (Doc++-Group: GHMM-Globals) */
+
