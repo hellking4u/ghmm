20cd8eb1355e3058f434a9e0d8feff93a64e59e7
wasinee <wasinee@4362c293-2511-481e-a17b-8864836be22b>
2003-11-11 19:36:53 +0000

    Added Files: 	sdfoba.c sdfoba.h ----------------------------------------------------------------------

---
+++ ghmm/sdfoba.h
@@ -0,0 +1,33 @@
+/*******************************************************************************
+  author       : Utz Pape and Andrea Weisse
+  filename     : ghmm/ghmm/sdfoba.h
+
+  $Id$
+
+__copyright__
+
+*******************************************************************************/
+
+
+#ifndef SDFOBA_H
+#define SDFOBA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**@name HMM-Modell */
+/*@{ (Doc++-Group: model) */
+
+#include "ghmm/sdmodel.h"
+#include <math.h>
+#include "ghmm/const.h"
+#include "ghmm/matrix.h"
+#include "ghmm/mes.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SDFOBA_H
