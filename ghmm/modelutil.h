38b27886f3d6cefe0a6aa891dea72077e1e3f869
wasinee <wasinee@4362c293-2511-481e-a17b-8864836be22b>
2003-12-19 18:01:55 +0000

    *** empty log message ***

---
+++ ghmm/modelutil.h
@@ -0,0 +1,26 @@
+/*******************************************************************************
+  filename     : ghmm/ghmm/modelutil.h
+  created      : TIME: 10:47:27     DATE: Fri 19. December 1997
+  $Id$
+
+__copyright__
+
+*******************************************************************************/
+
+#ifndef GHMM_MODELUTIL_H
+#define GHMM_MODELUTIL_H
+
+#include "ghmm.h"
+#include "model.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void model_topo_ordering(model *mo);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
