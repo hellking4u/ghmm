2eebd32f2103c62c6e9297c8438aae9798d33fdf
cic99 <cic99@4362c293-2511-481e-a17b-8864836be22b>
2004-08-30 14:09:15 +0000

    added discrime.c and discrime.h

---
+++ ghmm/discrime.h
@@ -0,0 +1,63 @@
+/*******************************************************************************
+  author       : Janne Grunau
+  filename     : ghmm/ghmm/discrime.h
+  created      : TIME: 02:04:35     DATE: Sun 20. June 2004
+  $Id$
+
+__copyright__
+
+*******************************************************************************/
+
+
+#ifndef DISCRIME_H
+#define DISCRIME_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+model** discrime_modelarray_alloc(int size);
+void discrime_modelarray_dealloc(model** mos);
+
+void discrime_modelarray_setptr(model** mos, model* mo, int pos);
+model* discrime_modelarray_getptr(model** mos, int pos);
+
+sequence_t** discrime_seqarray_alloc(int size);
+void discrime_seqarray_dealloc(sequence_t** seqs);
+
+void discrime_seqarray_setptr(sequence_t** seqs, sequence_t* seq, int pos);
+sequence_t* discrime_seqarray_getptr(sequence_t** seqs, int pos);
+
+/*----------------------------------------------------------------------------*/
+/**
+   Trains two or more models to opimise the discrimination between the
+   classes in the trainingset.
+   The models must have the same topology. (checked)
+   @return                 0/-1 success/error
+   @param mo:              array of pointers to some models
+   @param sqs:             array of annotated sequence sets
+   @param noC:             number of classes
+   @param gradient:        if gradient == 0 try a closed form solution
+                           otherwise a gradient descent
+ */
+extern int discriminative(model** mo, sequence_t** sqs, int noC, int gradient);
+
+/*----------------------------------------------------------------------------*/
+/**
+   Returns the value of the in this discriminative training algorithm optimised
+   function for a tupel of HMMs and sequencesets.
+   @return                 value of funcion
+   @param mo:              array of pointers to some models
+   @param sqs:             array of annotated sequence sets
+   @param noC:             number of classes
+*/
+double discrime_compute_performance(model** mo, sequence_t** sqs, int noC);
+
+void discrime_print_statistics(model** mo, sequence_t** sqs, int noC,
+			       int* falseP, int* falseN);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
