f8cc269713725a0fff77d4f37ca2f9b006614ee5
cic99 <cic99@4362c293-2511-481e-a17b-8864836be22b>
2004-11-04 16:10:35 +0000

    *** empty log message ***

---
+++ trunk/ghmm/ghmmwrapper/class_change.py
@@ -0,0 +1,14 @@
+def getClass( seq, k,t):
+     print "Python: Inside of getClass. Argument value is "+str(t)
+     #print smo
+     #print seq
+     #print t
+     #print k
+     
+     if (t < 500):
+        res = 0
+     else:
+        res = 1   
+
+     print "Python: Return value: "+str(res)
+     return res
