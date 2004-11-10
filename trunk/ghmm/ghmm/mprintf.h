4f903ec09460660f4fbfa787688144e4802a88ef
schliep <schliep@4362c293-2511-481e-a17b-8864836be22b>
2004-11-10 14:55:33 +0000

    Update Copyright notice and headers

--- trunk/ghmm/ghmm/mprintf.h
+++ trunk/ghmm/ghmm/mprintf.h
@@ -1,4 +1,39 @@
 /*******************************************************************************
+*
+*       This file is part of the General Hidden Markov Model Library,
+*       GHMM version __VERSION__, see http://ghmm.org
+*
+*       Filename: ghmm/ghmm/mprintf.h
+*       Authors:  Frank Nuebel
+*
+*       Copyright (C) 1998-2004 Alexander Schliep 
+*       Copyright (C) 1998-2001 ZAIK/ZPR, Universitaet zu Koeln
+*	Copyright (C) 2002-2004 Max-Planck-Institut fuer Molekulare Genetik, 
+*                               Berlin
+*                                   
+*       Contact: schliep@ghmm.org             
+*
+*       This library is free software; you can redistribute it and/or
+*       modify it under the terms of the GNU Library General Public
+*       License as published by the Free Software Foundation; either
+*       version 2 of the License, or (at your option) any later version.
+*
+*       This library is distributed in the hope that it will be useful,
+*       but WITHOUT ANY WARRANTY; without even the implied warranty of
+*       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*       Library General Public License for more details.
+*
+*       You should have received a copy of the GNU Library General Public
+*       License along with this library; if not, write to the Free
+*       Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+*
+*       This file is version $Revision$ 
+*                       from $Date$
+*             last change by $Author$.
+*
+*******************************************************************************/
+/*******************************************************************************
   author       : Frank Nübel
   filename     : ghmm/ghmm/mprintf.h
   created      : TIME: 11:27:32     DATE: Wed 14. May 1997
