e29312ea672e094c2094e242b569460b296924c1
benrich <benrich@4362c293-2511-481e-a17b-8864836be22b>
2004-11-12 11:17:36 +0000

    Updated file header

--- trunk/ghmm/ghmm/gauss_tail.c
+++ trunk/ghmm/ghmm/gauss_tail.c
@@ -1,26 +1,37 @@
 /*******************************************************************************
-  author       : Bernhard Knab
-  filename     : ghmm/ghmm/gauss_tail.c
-  created      : March 2001 by Achim Gaedke from hmm/src/creestimate.c
-  $Id$
-
-Copyright (C) 1998-2001, ZAIK/ZPR, Universität zu Köln
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Library General Public
-License as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Library General Public License for more details.
-
-You should have received a copy of the GNU Library General Public
-License along with this library; if not, write to the Free
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-
+*
+*       This file is part of the General Hidden Markov Model Library,
+*       GHMM version __VERSION__, see http://ghmm.org
+*
+*       Filename: ghmm/ghmm/gauss_tail.c
+*       Authors:  Bernhard Knab
+*
+*       Copyright (C) 1998-2004 Alexander Schliep
+*       Copyright (C) 1998-2001 ZAIK/ZPR, Universitaet zu Koeln
+*       Copyright (C) 2002-2004 Max-Planck-Institut fuer Molekulare Genetik,
+*                               Berlin
+*
+*       Contact: schliep@ghmm.org
+*
+*       This library is free software; you can redistribute it and/or
+*       modify it under the terms of the GNU Library General Public
+*       License as published by the Free Software Foundation; either
+*       version 2 of the License, or (at your option) any later version.
+*
+*       This library is distributed in the hope that it will be useful,
+*       but WITHOUT ANY WARRANTY; without even the implied warranty of
+*       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*       Library General Public License for more details.
+*
+*       You should have received a copy of the GNU Library General Public
+*       License along with this library; if not, write to the Free
+*       Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+*
+*       This file is version $Revision$
+*                       from $Date$
+*             last change by $Author$.
+*
 *******************************************************************************/
 
 #include <float.h>
