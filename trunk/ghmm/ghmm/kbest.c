7f872cb27da2c8609a02eab5fc02b0e55e1a5cad
grunau <grunau@4362c293-2511-481e-a17b-8864836be22b>
2005-02-23 15:53:28 +0000

    removed quadratic complexity on the number of states use mes.h framework

--- trunk/ghmm/ghmm/kbest.c
+++ trunk/ghmm/ghmm/kbest.c
@@ -35,35 +35,37 @@
 *******************************************************************************/
 
 #include <stdlib.h>
-#include "model.h"
-#include "kbest.h"
-#include "kbestbasics.h"
 #include <math.h>
+#include "mes.h"
+#include "model.h"
+#include "kbestbasics.h"
+#include "kbest.h"
 
 
 /**
   Builds logarithmic transition matrix from the states' in_a values
+  the row for each state is the logarithmic version of the state's in_a
   @return transition matrix with logarithmic values, 1.0 if a[i,j] = 0
   @param s:           array of all states of the model
   @param N:           number of states in the model
  */
-inline double* buildLogMatrix(state* s, int N) {
+double** kbest_buildLogMatrix(state* s, int N) {
+#define CUR_PROC "kbest_buildLogMatrix"
   int i,j;
-  double* log_a;      /* log(a(i,j)) => log_a[i*N+j] */
+  double** log_a;      /* log(a(i,j)) => log_a[i*N+j] */
 
   /* create & initialize matrix: */
-  log_a = (double*)malloc(sizeof(double)*N*N);
-  for (i=0; i<N; i++)
-    for (j=0; j<N; j++)
-      log_a[i*N+j]=1.0; /* positive values cannot occur as logarithms */
-
-  for (i=0; i<N; i++)
-    for (j=0; j<s[i].in_states; j++) {
-      if (s[i].in_a[j] > KBEST_EPS)  /* only accept values greater than EPS */
-	log_a[s[i].in_id[j]*N+i] = log(s[i].in_a[j]);
-    }
-
+  if (!m_malloc(log_a, N)) {mes_proc(); goto STOP;}
+  for (i=0; i<N; i++) {
+    if (!m_malloc(log_a[i], s[i].in_states)) {mes_proc(); goto STOP;}
+    for (j=0; j<s[i].in_states; j++)
+      log_a[i][j] = log(s[i].in_a[j]);
+  }
   return log_a;
+STOP:
+  mes_prot("kbest_buildLogMatrix failed\n");
+  exit(1);
+#undef CUR_PROC
 }
 
 
@@ -80,14 +82,16 @@ inline double* buildLogMatrix(state* s, int N) {
    @param log_p:      variable reference to store the log prob. of the labeling
  */
 int* kbest(model* mo, int* o_seq, int seq_len, int k, double* log_p) {
+#define CUR_PROC "kbest"
   int i, t, c, l, m;		/* counters */
   int no_oldHyps;		/* number of hypotheses until position t-1 */
-  int b_index;			/* index for addressing states' b arrays */
+  int b_index, i_id;            /* index for addressing states' b arrays */
   int no_labels=0;
+  int exists, g_nr;
 
   /* logarithmized transition matrix A, log(a(i,j)) => log_a[i*N+j],
        1.0 for zero probability */
-  double* log_a;
+  double** log_a;
 
   /* matrix of hypotheses, holds for every position in the sequence a list
      of hypotheses */
@@ -96,7 +100,6 @@ int* kbest(model* mo, int* o_seq, int seq_len, int k, double* log_p) {
   
   /* vectors for rows in the matrices */
   int *hypothesis;
-  double* gammaRow;
   
   /* pointer & prob. of the k most probable hypotheses for each state
        - matrices of dimensions #states x k:  argm(i,l) => argmaxs[i*k+l] */
@@ -110,7 +113,7 @@ int* kbest(model* mo, int* o_seq, int seq_len, int k, double* log_p) {
   /* break if sequence empty or k<1: */
   if (seq_len <= 0 || k<=0) return NULL;
 
-  h = (hypoList**)calloc(seq_len, sizeof(hypoList*));
+  if (!m_calloc(h, seq_len)) {mes_proc(); goto STOP;}
 
   /** 1. Initialization (extend empty hypothesis to #labels hypotheses of
          length 1): */
@@ -123,38 +126,52 @@ int* kbest(model* mo, int* o_seq, int seq_len, int k, double* log_p) {
   no_labels++;
 
   /* initialize h: */
-  h[0] = NULL;
-  for (c=no_labels-1; c>=0; c--) {
-    /* create #labels hypotheses of max. length seq_len: */
-    hlist_insertElem(&(h[0]), c, NULL);
-    /* create #hypotheses rows in gamma of length #states: */
-    gammaRow = (double*)malloc(sizeof(double)*mo->N);
-    for(i=0; i < mo->N; i++) {
-      /* if hypothesis c ends with label of state i:
-	   gamma(i,c):= log(pi[i]*b[i](o_seq[0]))
-	 else: gamma(i,c):= -INF (represented by 1.0)
-      */
-      if (c == mo->s[i].label) {
-      	b_index = get_emission_index(mo,i,o_seq[0],0);
-      	if (b_index < 0 || mo->s[i].pi < KBEST_EPS || mo->s[i].b[b_index] < KBEST_EPS)
-	  gammaRow[i] = 1.0;
-        else
-	  gammaRow[i] = log(mo->s[i].pi) + log(mo->s[i].b[b_index]);
+  hP = h[0];
+  for (i=0; i < mo->N; i++) {
+    if (mo->s[i].pi > KBEST_EPS) {
+      exists = 0;
+      while (hP != NULL) {
+	if (hP->hyp_c == mo->s[i].label) {
+	  /* add entry to the gamma list */
+	  g_nr = hP->gamma_states;
+	  hP->gamma_id[g_nr] = i;
+	  hP->gamma_a[g_nr]  = log(mo->s[i].pi) + log(mo->s[i].b[get_emission_index(mo,i,o_seq[0],0)]);
+	  hP->gamma_states   = g_nr+1;
+	  /*hlist_add_gamma_entry(hP, i, log(mo->s[i].pi) + log(mo->s[i].b[get_emission_index(mo,i,o_seq[0],0)]));*/
+	  exists = 1;
+	  break;
+	}
+	else hP = hP->next;
       }
-      else gammaRow[i] = 1.0;
+      if (!exists) {
+	hlist_insertElem(&(h[0]), mo->s[i].label, NULL);
+	/* initiallize gamma-array with safe size (number of states) and add the first entry */
+	if (!m_malloc(h[0]->gamma_a,  mo->N)) {mes_proc(); goto STOP;}
+	if (!m_malloc(h[0]->gamma_id, mo->N)) {mes_proc(); goto STOP;}
+	h[0]->gamma_id[0]  = i;
+	h[0]->gamma_a[0]   = log(mo->s[i].pi) + log(mo->s[i].b[get_emission_index(mo,i,o_seq[0],0)]);
+	h[0]->gamma_states = 1; 
+	/*hlist_add_gamma_entry(h[0], i, log(mo->s[i].pi) + log(mo->s[i].b[get_emission_index(mo,i,o_seq[0],0)]));*/
+	h[0]->chosen = 1;
+      }
+      hP = h[0];
     }
-    /* save the gamma values in the hypotheses and
-       chose all initial hypotheses */
-    h[0]->gamma = gammaRow;
-    h[0]->chosen = 1;
+  }
+  /* reallocating the gamma list to the real size */
+  hP = h[0];
+  while (hP != NULL) {
+    if (m_realloc(hP->gamma_a,  hP->gamma_states)) {mes_proc(); goto STOP;}
+    if (m_realloc(hP->gamma_id, hP->gamma_states)) {mes_proc(); goto STOP;}
+    hP = hP->next;
   }
 
   /* calculate transition matrix with logarithmic values: */
-  log_a = buildLogMatrix(mo->s, mo->N);
+  log_a = kbest_buildLogMatrix(mo->s, mo->N);
 
   /* initialize temporary arrays: */
   maxima=(double*)malloc(sizeof(double)*mo->N*k);     /* for each state save k */
   argmaxs=(hypoList**)malloc(mo->N*k*sizeof(hypoList*));
+
 
   /*------ Main loop: Cycle through the sequence: ------*/
   for (t=1; t<seq_len; t++) {
@@ -163,28 +180,30 @@ int* kbest(model* mo, int* o_seq, int seq_len, int k, double* log_p) {
     update_emission_history(mo,o_seq[t-1]);
     
     /** 2. Propagate hypotheses forward and update gamma: */
-    no_oldHyps = propFwd(h[t-1], &(h[t]), no_labels, seq_len);
+    no_oldHyps = hlist_propFwd(mo, h[t-1], &(h[t]), no_labels);
+    /*printf("t = %d (%d), no of old hypotheses = %d\n", t, seq_len, no_oldHyps);*/
     
     /*-- calculate new gamma: --*/
     hP = h[t];
     /* cycle through list of hypotheses */
     while (hP != NULL) {
-      hP->gamma = (double*)malloc(mo->N*sizeof(double));
-      for (i=0; i < mo->N; i++) {
-      	if (mo->s[i].label == hP->hyp_c) {
-	  /* if hypothesis hP ends with label of state i:
-	       gamma(i,c):= log(sum(exp(a(j,i)*exp(oldgamma(j,old_c)))))
-	                      + log(b[i](o_seq[t]))
-	     else: gamma(i,c):= -INF (represented by 1.0)
-	  */
-      	  hP->gamma[i] = logGammaSum(log_a, i, mo->N, hP->parent->gamma);
-	  b_index=get_emission_index (mo, i, o_seq[t], t);
-      	  if (b_index<0 || hP->gamma[i]==1.0 || mo->s[i].b[b_index]<KBEST_EPS)
-	    hP->gamma[i] = 1.0;
-	  else 
-	    hP->gamma[i] += log(mo->s[i].b[b_index]);
+
+      for (i=0; i<hP->gamma_states; i++) {
+	/* if hypothesis hP ends with label of state i:
+	   gamma(i,c):= log(sum(exp(a(j,i)*exp(oldgamma(j,old_c)))))
+	                + log(b[i](o_seq[t]))
+	   else: gamma(i,c):= -INF (represented by 1.0) */
+	i_id = hP->gamma_id[i];
+	hP->gamma_a[i] = logGammaSum(log_a[i_id], &mo->s[i_id], hP->parent);
+	b_index=get_emission_index (mo, i_id, o_seq[t], t);
+	if (b_index<0) {
+	  mes_prot("invalid emission index! kbest failed\n");
+	  exit(1);
 	}
-	else hP->gamma[i] = 1.0;
+	else 
+	  hP->gamma_a[i] += log(mo->s[i_id].b[b_index]);
+	/*printf("%g = %g\n", log(mo->s[i_id].b[b_index]), hP->gamma_a[i]);*/
+	if (hP->gamma_a[i]>0.0) {mes_prot("gamma to large. kbest failed\n"); exit(1);}
       }
       hP = hP->next;
     }
@@ -202,19 +221,20 @@ int* kbest(model* mo, int* o_seq, int seq_len, int k, double* log_p) {
        each state: */
     hP=h[t];
     while (hP != NULL) {
-      for (i=0; i < mo->N; i++) {
-	if (hP->gamma[i]>KBEST_EPS) continue;
+      for (i=0; i<hP->gamma_states; i++) {
+	i_id = hP->gamma_id[i];
+	if (hP->gamma_a[i] > KBEST_EPS) continue;
 	/* find first best hypothesis that is worse than current hypothesis: */
-	for (l=0; l<k && maxima[i*k+l] < KBEST_EPS && maxima[i*k+l] > hP->gamma[i]; l++);
+	for (l=0; l<k && maxima[i_id*k+l] < KBEST_EPS && maxima[i_id*k+l] > hP->gamma_a[i]; l++);
 	if (l<k) {
 	  /* for each m>l: m'th best hypothesis becomes (m+1)'th best */
 	  for (m=k-1; m>l; m--) {
-	    argmaxs[i*k+m] = argmaxs[i*k+m-1];
-	    maxima[i*k+m] = maxima[i*k+m-1];
+	    argmaxs[i_id*k+m] = argmaxs[i_id*k+m-1];
+	    maxima[i_id*k+m] = maxima[i_id*k+m-1];
 	  }
 	  /* save new l'th best hypothesis: */
-	  maxima[i*k+l]  = hP->gamma[i];
-	  argmaxs[i*k+l] = hP;
+	  maxima[i_id*k+l]  = hP->gamma_a[i];
+	  argmaxs[i_id*k+l] = hP;
 	}
       }
       hP=hP->next;
@@ -231,6 +251,7 @@ int* kbest(model* mo, int* o_seq, int seq_len, int k, double* log_p) {
     while (h[t] != NULL && 0==h[t]->chosen)
       hlist_removeElem(&(h[t]));
     /* remove all other not chosen hypotheses */
+    if (!h[t]) {mes_prot("No chosen hypothesis. kbest failed\n"); exit(1);}
     hP=h[t];
     while (hP->next != NULL) {
       if (1==hP->next->chosen)
@@ -242,7 +263,8 @@ int* kbest(model* mo, int* o_seq, int seq_len, int k, double* log_p) {
   /* dispose of temporary arrays: */
   free(argmaxs);
   free(maxima);
-  free(log_a);    /* transition matrix is no longer needed from here on */
+  /* transition matrix is no longer needed from here on */
+  free(log_a);
   
   /** 4. Save the hypothesis with the highest probability over all states: */
   hP=h[seq_len-1];
@@ -250,7 +272,7 @@ int* kbest(model* mo, int* o_seq, int seq_len, int k, double* log_p) {
   *log_p = 1.0;   /* log_p will store log of maximum summed probability */
   while (hP != NULL) {
     /* sum probabilities for each hypothesis over all states: */
-    sum=logSum(hP->gamma,mo->N);
+    sum=logSum(hP->gamma_a, hP->gamma_states);
     /* and select maximum sum */
     if (sum < KBEST_EPS && (*log_p==1.0 || sum>*log_p)) {
       *log_p=sum;
@@ -262,7 +284,7 @@ int* kbest(model* mo, int* o_seq, int seq_len, int k, double* log_p) {
   /* found a valid path? */
   if (*log_p < KBEST_EPS) {
     /* yes: extract chosen hypothesis: */
-    hypothesis=(int*)malloc(sizeof(int)*seq_len);
+    if (!m_malloc(hypothesis, seq_len)) {mes_proc(); goto STOP;}
     for (i=seq_len-1; i>=0; i--) {
       hypothesis[i] = argmax->hyp_c;
       argmax = argmax->parent;
@@ -278,6 +300,8 @@ int* kbest(model* mo, int* o_seq, int seq_len, int k, double* log_p) {
     hlist_removeElem(&hP);
   free(h);
   return hypothesis;
+STOP:
+  mes_prot("kbest failed\n");
+  exit(1);
+#undef CUR_PROC
 }
-
-
