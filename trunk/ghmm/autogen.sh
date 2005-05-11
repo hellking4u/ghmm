491c5a74fc662035bc4c63e2c98a0f0b7f08c3b2
schliep <schliep@4362c293-2511-481e-a17b-8864836be22b>
2005-05-11 14:42:45 +0000

    Changed line order in autogen.sh. Declared bw_one_step static to match sreestimate.c.

--- trunk/ghmm/autogen.sh
+++ trunk/ghmm/autogen.sh
@@ -10,17 +10,17 @@ for f in $sep_path ; do
   test -f $f/m4 && echo `eval $f/m4 --version 2>/dev/null` |grep GNU >/dev/null && PATH=$f:$PATH
 done
 
+#creates ltmain.sh (Call first!!!)
+libtoolize --automake --force --copy
+
 #makes aclocal.m4 from acinclude.m4 and other files
 aclocal
 
 #scans configure.in and creates config.h.in
-autoheader
+autoheader 
 
 #creates Makefile.in from Makefile.am
-automake --add-missing
-
-#creates ltmain.sh
-libtoolize --automake
+automake --add-missing --force-missing
 
 #creates configure from configure.in
 autoconf
