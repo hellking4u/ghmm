9cd0a7ea41eb09928b79791ed9718ce5e3a33e46
cic99 <cic99@4362c293-2511-481e-a17b-8864836be22b>
2005-09-07 10:23:31 +0000

    minor bug

--- ghmmwrapper/test_switch.py
+++ ghmmwrapper/test_switch.py
@@ -37,30 +37,116 @@ import ghmm
 import ghmmwrapper
 import ghmmhelper
 
+
+
+
  # Interpretation of B matrix for the mixture case (Example with three states and two components each):
                 #  B = [ 
                 #      [ ["mu11","mu12"],["sig11","sig12"],["w11","w12"]   ],
                 #      [  ["mu21","mu22"],["sig21","sig22"],["w21","w22"]  ],
                 #      [  ["mu31","mu32"],["sig31","sig32"],["w31","w32"]  ],
                 #      ]
+
+
+
+F = ghmm.Float()            		   
+m2 = ghmm.HMMFromMatrices(F,ghmm.GaussianMixtureDistribution(F),
+                         [[0.0,1.0,0],[0.5,0.0,0.5],[0.3,0.3,0.4]],
+                         
+                         [
+                         [ [0.0,1.0],[1.0,0.5], [0.6,0.4] ],
+                         [ [2.0,1.0],[1.0,0.5], [0.8,0.2] ],
+                         [ [15.0,1.0],[1.0,0.5], [0.8,0.2] ]
+                         ],
+                         
+                         [1.0,0,0])
+
+
+
+
+
+A = [[0.9,0.1],[0.2,0.8]]
+B = [ [[0.0,0.0],[0.1,0.1],[0.5,0.5]],
+    [[10.0,10.0],[0.1,0.1],[0.5,0.5]] ]
+pi = [0.0,1.0]
+model = ghmm.HMMFromMatrices(ghmm.Float(),ghmm.GaussianMixtureDistribution(ghmm.Float), A, B, pi)
+
+#short = ghmm.EmissionSequence(ghmm.Float(),[0.0,10.0,10.0,0.0,10.0,0.0,10.0,10.0,0.0])
+#short2 = ghmm.EmissionSequence(ghmm.Float(),[10.0,0.0]*6)
+
+
+#print model
+#model.baumWelch(short2,1,0.2)
+#print model
+
+#def getClass(seq, k,t):
+#    classes = [0] * 10 + [1] * 10
+#    return classes[t]
+
+
+swA = [
+    [[0.9,0.1],[0.2,0.8]],
+    [[0.3,0.7],[0.7,0.3]]
+    ]
+
+swB = [ [[0.0,0.0],[0.1,0.1],[0.5,0.5]],
+    [[10.0,10.0],[0.1,0.1],[0.5,0.5]] ]
+swpi = [0.5,0.5]
+swmodel = ghmm.HMMFromMatrices(ghmm.Float(),ghmm.GaussianMixtureDistribution(ghmm.Float), swA, swB, swpi)
+
+ghmmwrapper.smodel_class_change_alloc(swmodel.cmodel)
+ghmmwrapper.setPythonSwitching(swmodel.cmodel,"class_change","getClass")   # "class_change"
+
+swshort = ghmm.EmissionSequence(ghmm.Float(),[10.0]+[0.0]*5+[0.0]+[10.0]*5)
+
+print swshort
+
+print swmodel
+swmodel.baumWelch(swshort,5,0.2)
+print swmodel
+
+
+
+
+
+
+
+
+
+
+
+
+
+#A = [ [[0.0,1.0],[0.0,0.0]],
+#      [[1.0,0.0],[1.0,0.0]]
+#    ]
+
+
                 
-A2 = [ [[0.0,1.0,0.0],[0.0,0.0,1.0],[1.0,0.0,0.0]],
-      [[1.0,0.0,0.0],[1.0,0.0,0.0],[1.0,0.0,0.0]] ]
+#A2 = [ [[0.0,1.0,0.0],[0.0,0.0,1.0],[1.0,0.0,0.0]],
+#      [[1.0,0.0,0.0],[1.0,0.0,0.0],[1.0,0.0,0.0]] ]
      
-B2 = [ [[0.0,0.000001],[0.4,0.4],[0.5,0.5]],
-    [[1.0,0.000001],[0.3,0.3],[0.5,0.5]],
-     [[2.0,0.000001],[0.8,0.8],[0.5,0.5]] ]
+#B2 = [ [[0.0,0.000001],[0.4,0.4],[0.5,0.5]],
+#    [[1.0,0.000001],[0.3,0.3],[0.5,0.5]],
+#     [[2.0,0.000001],[0.8,0.8],[0.5,0.5]] ]
 
 
-pi2 = [1.0,0.0,0.0]
-model2 = ghmm.HMMFromMatrices(ghmm.Float(),ghmm.GaussianMixtureDistribution(ghmm.Float), A2, B2, pi2)
+#pi2 = [1.0,0.0,0.0]
+#model2 = ghmm.HMMFromMatrices(ghmm.Float(),ghmm.GaussianMixtureDistribution(ghmm.Float), A2, B2, pi2)
 
-ghmmwrapper.smodel_class_change_alloc(model2.cmodel)
+#ghmmwrapper.smodel_class_change_alloc(model2.cmodel)
+#ghmmwrapper.setPythonSwitching(hmm.cmodel,"class_change","getClass")
 
 #ghmmwrapper.setPythonSwitching(model2.cmodel,"class_change","getClass")
-ghmmwrapper.setSwitchingFunction(model2.cmodel)
+#ghmmwrapper.setSwitchingFunction(model2.cmodel)
 
 #print model2
-seq = model2.sample(2,30)
+#seq = model2.sample(2,30)
 
-model2.write("test_switch.smo")
+#seq = EmissonSequence(ghmm.Float(),
+
+
+
+#model2.write("test_switch.smo")
+#m2 = ghmm.HMMOpen("test_switch.smo")
+#print m2
