9f9b67bbff1b70d0ae777d9447d7acc41a48659f
grunau <grunau@4362c293-2511-481e-a17b-8864836be22b>
2005-11-23 16:21:27 +0000

    added Wasinee's libxml-test code to tests change configure and Makefile to include libxml2 headers and link against it

---
+++ trunk/ghmm/tests/libxml-test.c
@@ -0,0 +1,201 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <libxml/xmlmemory.h>
+#include <libxml/parser.h>
+
+
+int getIntAttribute(xmlNodePtr node, const xmlChar *name, int *error)
+{
+  xmlChar *attr;
+  int value = 0;
+
+  if( (attr = xmlGetProp(node, name)) != NULL) {
+    value = atoi((char*)attr);
+    xmlFree(attr);
+    *error = 0;
+  } else {
+    *error = 1;
+  }
+  return value;
+}
+
+/* Caller owns return value */
+xmlChar* getXMLCharAttribute(xmlNodePtr node, const xmlChar *name, int *error)
+{
+  xmlChar *attr;
+
+  if( (attr = xmlGetProp(node, name)) != NULL) {
+    *error = 0;
+    return attr;
+  } else {
+    *error = 1;
+    return NULL;
+  }
+}
+
+static void 
+parseNode(xmlDocPtr doc, xmlNodePtr cur)
+{
+  int error;
+  xmlChar *stuff;
+  xmlNode *data = NULL;
+  xmlChar* dataKey;
+  char *s, *tok;
+
+  /* Handle Node Data */
+  for( data = cur->xmlChildrenNode; data != NULL; data = data->next) {
+
+    if(data->type == XML_ELEMENT_NODE) {
+      if( !xmlStrcmp(data->name, (const xmlChar*)"data")) {
+	dataKey = getXMLCharAttribute(data,(const xmlChar*)"key",&error);
+	printf("  Data key=%s\n", (char*)dataKey);
+	    
+	/* Collect the value */
+	stuff = xmlNodeListGetString(doc, data->xmlChildrenNode, 1);
+
+	if (strcmp( (char*)dataKey, "emissions" ) == 0) {
+	  /* parse emission vector */
+	  s = (char *) stuff;
+	  tok = strtok(s, ",");
+	  do {
+	    printf( "  %s ", tok);
+	    tok = strtok(NULL, ",");
+	  } while ( tok != NULL );
+	  printf( "\n" );
+	} else {
+	  printf("  value=%s\n", (char*)stuff);
+	}
+
+	xmlFree(stuff);
+	xmlFree(dataKey);
+      }
+    }
+  }
+}
+
+static void
+parseGraph(xmlDocPtr doc, xmlNodePtr cur)
+{
+  xmlChar *stuff;
+  xmlNode *data = NULL;
+  xmlChar* dataKey;
+  int i, id, error;
+  int source, target;
+
+  int N = 0; /* We don't follow C-style indexing strategy */ 
+  int *inDegree = NULL;  
+  int *outDegree = NULL;
+
+
+  cur = cur->xmlChildrenNode;
+
+  printf("parseGraph\n");
+  while( cur != NULL) {
+
+    /* ========== NODES ==================================================  */
+    if( (!xmlStrcmp(cur->name, (const xmlChar*)"node"))) {      
+      id = getIntAttribute(cur, (const xmlChar*)"id", &error); /*\ufffdGet the node ID */
+      if(!error)
+	printf("node id = %d\n", id);
+
+      N++;
+      parseNode(doc, cur);
+    }
+    
+    /* ========== EDGES ==================================================  */
+    if( (!xmlStrcmp(cur->name, (const xmlChar*)"edge"))) {      
+
+      if (inDegree == NULL) {
+	inDegree = (int *) calloc( N+1, sizeof(int));
+	outDegree = (int *) calloc(N+1, sizeof(int));
+	
+	memset(inDegree,  0, (N+1)*sizeof(int));
+	memset(outDegree, 0, (N+1)*sizeof(int));
+      }
+
+      source = getIntAttribute(cur, (const xmlChar*)"source", &error); /*\ufffdGet the node ID */
+      if(!error)
+	printf("source = %d\n", source);
+      else
+	return;
+
+      target = getIntAttribute(cur, (const xmlChar*)"target", &error); /*\ufffdGet the node ID */
+      if(!error)
+	printf("target = %d\n", target);      
+      else
+	return;
+      
+      inDegree[target] += 1;
+      outDegree[source] += 1;
+    }
+    cur = cur->next;
+  }
+
+  printf("Found HMM with %d states\n", N);
+  for(i = 1; i <= N; i++) {
+    printf("  %d\t%d\n", inDegree[i], outDegree[i]);
+  }
+  
+  free(inDegree);
+  free(outDegree);
+}
+
+
+static void
+parseHMMDocument(char *docname) {
+  
+  xmlDocPtr doc;
+  xmlNodePtr cur;
+
+  doc = xmlParseFile(docname);
+
+  if(doc == NULL) {
+    fprintf(stderr, "Document %s not parsed successfully.\n", docname);
+    return;
+  }
+  
+  cur = xmlDocGetRootElement(doc);
+
+  if(cur == NULL) {
+    fprintf(stderr, "Document %s is empty\n", docname); 
+    xmlFreeDoc(doc);
+    return;
+  }
+
+  if( xmlStrcmp(cur -> name, (const xmlChar*)"graphml")) {
+    fprintf(stderr, "Document %s is not of type 'graphml'\n", docname); 
+    xmlFreeDoc(doc);
+    return;
+  }
+
+  cur = cur->xmlChildrenNode;
+
+  while( cur != NULL) {
+    printf("'%s'\n", (const char*)cur->name);
+    if( !xmlStrcmp(cur->name, (const xmlChar*)"graph")) {
+      parseGraph(doc, cur);
+    }
+    cur = cur -> next;
+  }
+
+  xmlFreeDoc(doc);
+  return;
+}
+
+
+int
+main(int argc, char **argv) {
+
+  char *docname;
+
+  if(argc <= 1) {
+    printf("Usage: %s docname.xml", argv[0]);
+    return(0);
+  }
+  docname = argv[1];
+  parseHMMDocument(docname);
+  
+  return(1);
+}
