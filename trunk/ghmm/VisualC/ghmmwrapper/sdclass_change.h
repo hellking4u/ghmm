c3ba65950b2cbae40a5c8f139b23ef1ca3995fef
grunau <grunau@4362c293-2511-481e-a17b-8864836be22b>
2008-04-01 15:17:59 +0000

    remove C++ wrapper and VisialC files

--- trunk/ghmm/VisualC/ghmmwrapper/sdclass_change.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ghmm/rng.h>
-#include <ghmm/sequence.h>
-#include <ghmm/sdmodel.h>
-
-int cp_class_change(int *seq, int len);
-void setSwitchingFunction( sdmodel *smd );
