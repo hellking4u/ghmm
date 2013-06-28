e8b6dedb665b40da7e071181d48f8cce97fa3943
ejb177 <ejb177@4362c293-2511-481e-a17b-8864836be22b>
2013-06-28 16:16:46 +0000

    edited fbgibbs

--- ghmm/fbgibbs.h
+++ ghmm/fbgibbs.h
@@ -9,6 +9,10 @@ extern "C" {
 //warnining work in progress
 //**uses gsl**
 
+int sample(int seed, double* dist, int N);
+
+void update(int seed, ghmm_dmodel* mo, int T, int *states, int* O, double **pA, double **pB, double *pPi);
+
 void ghmm_dmodel_fbgibbstep (ghmm_dmodel * mo, int seed, int *O, int len, double **pA, double **pB, double *pPi, int* Q);
 
 void ghmm_dmodel_fbgibbs (ghmm_dmodel * mo, int seed, int *O, int len, double **pA, double **pB, double *pPi, int* Q, int burnIn); 
